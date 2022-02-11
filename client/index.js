b1486d0260c64603ca8de3007cd315ae75b6f835
Austin White <basketballking19aw@gmail.com>
2022-02-11 12:22:11 -0700

    first commit of test will need to improve later

---
+++ client/index.js
@@ -0,0 +1,62 @@
+document.getElementById("complimentButton").onclick = function () {
+    axios.get("http://localhost:4000/api/compliment/")
+        .then(function (response) {
+          const data = response.data;
+          alert(data);
+        });
+  };
+
+
+//   let btn = document.createElement("Button");
+// btn.innerHTML = "Fortune";
+// btn.addEventListener("click", function () {
+//   alert("Button is clicked");
+// });
+// document.body.appendChild(btn);
+
+
+ document.getElementById('fortuneButton').onclick = function () {
+    axios.get("http://localhost:4000/api/fortune/")
+    .then(function (response) {
+      const data = response.data;
+      alert(data);
+    });
+}
+
+document.getElementById('list').onclick = function () {
+    axios.post("http://localhost:4000/api/list/", )
+    .then(function (response) {
+      const data = response.data;
+      alert(data);
+    });
+}
+
+// I get errors when the code below runs so i have it not running at the moment I know I am not doing it right or missing something but Im just pretty stuck on this 
+
+// document.getElementById('delete').onclick = function () {
+//     axios.delete("http://localhost:4000/api/list/", )
+//     .then(function (response) {
+//       const data = response.data;
+//       alert(data);
+//     });
+// }
+
+
+// document.getElementById('updateId').onclick = function () {
+//     axios.put("http://localhost:4000/api/list/", )
+//     .then(function (response) {
+//       const data = response.data;
+//       alert(data);
+//     });
+// }
+
+
+
+
+// let user = {
+//     username: 'Dragon',
+//     firstName: 'Joey',
+//     lastName: 'Tribiani'
+//   }
+  
+//   axios.post('/api/users', user).then()
